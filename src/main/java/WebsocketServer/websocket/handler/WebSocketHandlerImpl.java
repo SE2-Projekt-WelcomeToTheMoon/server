dc2accfbbfa77d169e13e051f16f2453357922a7
kaiser.philipp95@icloud.com <130212301+kaiser1101@users.noreply.github.com>
2024-03-28 22:35:03 +0100

    new setup for server structure

--- src/main/java/WebsocketServer/websocket/handler/WebSocketHandlerImpl.java
+++ src/main/java/WebsocketServer/websocket/handler/WebSocketHandlerImpl.java
@@ -1,41 +1,18 @@
 package WebsocketServer.websocket.handler;
 
-
-import WebsocketServer.messaging.dtos.OutputMessage;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.socket.*;
-
-import java.util.LinkedList;
 
 public class WebSocketHandlerImpl implements WebSocketHandler {
 
-    LinkedList<WebSocketSession> connection = new LinkedList();
 
-
-    /**
-     * aufgerufen, wenn client sich versucht zu verbinden
-     * @param session
-     * @throws Exception
-     */
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-
-        connection.add(session);
-        System.out.println("Mit Server verbunden: "+ session.getId());
-
+        System.out.println("Verbindung erfolgreich hergestellt: " + session.getId());
     }
 
-    /**
-     * wird aufgereufen, wenn Server eine nachricht bekommt
-     */
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
         // TODO handle the messages here
-        String json = (String) message.getPayload();
-        ObjectMapper mapper = new ObjectMapper();
-        OutputMessage outputMessage = mapper.readValue(json, OutputMessage.class);
-
-
         session.sendMessage(new TextMessage("echo from handler: " + message.getPayload()));
     }
 
@@ -46,8 +23,7 @@ public class WebSocketHandlerImpl implements WebSocketHandler {
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
-        connection.remove(session);
-        System.out.println("Verbindung geschlossen: "+ session.getId());
+        System.out.println("Verbindung getrennt: "+ session.getId());
     }
 
     @Override
