8b2c4ca27e5e0e2aff916d15f5e31c9ba7b5301d
julian <julianlechner31@gmail.com>
2024-04-05 16:46:21 +0200

    Implemented Testcases for class GameBoard to check if isFinalized is checked and modified correctly.

--- src/test/java/WebsocketServer/game/model/GameBoardTest.java
+++ src/test/java/WebsocketServer/game/model/GameBoardTest.java
@@ -2,6 +2,7 @@ package WebsocketServer.game.model;
 
 import WebsocketServer.game.enums.FieldCategory;
 import WebsocketServer.game.enums.FieldValue;
+import WebsocketServer.game.exceptions.FinalizedException;
 import WebsocketServer.game.exceptions.FloorSequenceException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,15 +11,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameBoardTest {
     private GameBoard gameBoard;
-
+    private Floor floor;
     @BeforeEach
     void setUp() {
         gameBoard = new GameBoard();
+        floor = new Floor(FieldCategory.ROBOTER);
     }
 
     @Test
     void testAddFloor() {
-        Floor floor = new Floor(FieldCategory.ROBOTER);
         gameBoard.addFloor(floor);
 
         gameBoard.finalizeGameBoard();
@@ -39,7 +40,6 @@ class GameBoardTest {
 
     @Test
     void testSetAndGetFieldValueWithinFloorAtIndex() {
-        Floor floor = new Floor(FieldCategory.ROBOTER);
         Chamber chamber = new Chamber(FieldCategory.ROBOTER);
         Field field = new Field(FieldCategory.ROBOTER, FieldValue.ONE);
         chamber.addField(field);
@@ -54,7 +54,6 @@ class GameBoardTest {
 
     @Test
     void testSetInvalidValueWithinFloorAtIndex() {
-        Floor floor = new Floor(FieldCategory.ROBOTER);
         Chamber chamber = new Chamber(FieldCategory.ROBOTER);
         Field field1 = new Field(FieldCategory.ROBOTER, FieldValue.ONE);
         Field field2 = new Field(FieldCategory.ROBOTER, FieldValue.THREE);
@@ -76,5 +75,50 @@ class GameBoardTest {
             gameBoard.finalizeGameBoard();
             gameBoard.getFloorAtIndex(-1);
         });
+    }
+
+    @Test
+    public void testIsFinalizedInitiallyFalse() {
+        assertFalse(gameBoard.isFinalized());
+    }
+
+    @Test
+    public void testFinalizeGameBoardChangesIsFinalizedToTrue() {
+        gameBoard.finalizeGameBoard();
+        assertTrue(gameBoard.isFinalized());
+    }
+
+    @Test
+    public void testAddFloorAfterGameBoardFinalizationThrowsException() {
+        gameBoard.finalizeGameBoard();
+        assertThrows(FinalizedException.class, () -> gameBoard.addFloor(floor));
+    }
+
+    @Test
+    public void testGetFloorAtIndexBeforeGameBoardFinalizationThrowsException() {
+        gameBoard.addFloor(floor);
+        assertThrows(FinalizedException.class, () -> gameBoard.getFloorAtIndex(0));
+    }
+
+    @Test
+    public void testFinalizeGameBoardWithAlreadyFinalizedFloorThrowsException() {
+        floor.finalizeFloor();
+        gameBoard.addFloor(floor);
+        assertThrows(FinalizedException.class, () -> gameBoard.finalizeGameBoard());
+    }
+
+    @Test
+    public void testFinalizeGameBoardTwiceThrowsException() {
+        gameBoard.finalizeGameBoard();
+        assertThrows(FinalizedException.class, gameBoard::finalizeGameBoard);
+    }
+
+    @Test
+    public void testSetValueWithinFloorAtIndexBeforeGameBoardFinalizationThrowsException() {
+        Chamber chamber = new Chamber(FieldCategory.ROBOTER);
+        chamber.addField(new Field(FieldCategory.ROBOTER));
+        floor.addChamber(chamber);
+        gameBoard.addFloor(floor);
+        assertThrows(FinalizedException.class, () -> gameBoard.setValueWithinFloorAtIndex(0, 0, FieldValue.ONE));
     }
 }
