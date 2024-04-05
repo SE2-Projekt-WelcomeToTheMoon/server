dc5e4bde1588320aace031ce466523d28927bd37
kaiser.philipp95@icloud.com <130212301+kaiser1101@users.noreply.github.com>
2024-04-05 19:12:48 +0200

    creating basic funktion for lobby class

---
+++ src/main/java/WebsocketServer/game/lobby/Lobby.java
@@ -0,0 +1,38 @@
+
+package WebsocketServer.game.lobby;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lobby {
+
+    private static final int MAX_PLAYERS = 4;
+    private List<String> userList;
+
+    public Lobby(){
+        this.userList = new ArrayList<>();
+    }
+
+
+    /**
+     * Lobby Klasse mit basic implementierung
+     * Max Anzahl = 4
+     */
+    public boolean addPlayerToLobby(String username){
+        if(userList.size() < MAX_PLAYERS && !(userList.contains(username))){
+            userList.add(username);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void removePlayerFromLobbyByName(String username){
+        userList.remove(username);
+
+    }
+
+    public List<String> getUserListFromLobby(){
+        return new ArrayList<>(userList);
+    }
+
+}
