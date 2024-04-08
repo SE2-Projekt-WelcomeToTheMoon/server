05c03a28b7e2a13e5558496ee654c933e7275d97
thomas <t1wobak@edu.aau.at>
2024-04-08 15:09:03 +0200

    feat: Added CardCombination class for more clarity in getting combinations

---
+++ src/test/java/WebsocketServer/game/model/CardCombinationTest.java
@@ -0,0 +1,33 @@
+package WebsocketServer.game.model;
+
+import WebsocketServer.game.enums.FieldCategory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardCombinationTest {
+
+    @Test
+    public void testGetCurrentSymbol() {
+        PlayingCard card1 = new PlayingCard(FieldCategory.ROBOTER, 5);
+        PlayingCard card2 = new PlayingCard(FieldCategory.PFLANZE, 8);
+        CardCombination combination = new CardCombination(card1, card2);
+        assertEquals(FieldCategory.ROBOTER, combination.getCurrentSymbol());
+    }
+
+    @Test
+    public void testGetNextSymbol() {
+        PlayingCard card1 = new PlayingCard(FieldCategory.ROBOTER, 5);
+        PlayingCard card2 = new PlayingCard(FieldCategory.PFLANZE, 8);
+        CardCombination combination = new CardCombination(card1, card2);
+        assertEquals(FieldCategory.PFLANZE, combination.getNextSymbol());
+    }
+
+    @Test
+    public void testGetCurrentNumber() {
+        PlayingCard card1 = new PlayingCard(FieldCategory.ROBOTER, 5);
+        PlayingCard card2 = new PlayingCard(FieldCategory.PFLANZE, 8);
+        CardCombination combination = new CardCombination(card1, card2);
+        assertEquals(8, combination.getCurrentNumber());
+    }
+}
