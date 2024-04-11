a92f27289605cd9d8c6d800762858c063f428c1c
jublebi <julianlechner31@gmail.com>
2024-04-11 17:01:23 +0200

    Tested Player class

--- src/main/java/WebsocketServer/game/model/Player.java
+++ src/main/java/WebsocketServer/game/model/Player.java
@@ -1,9 +1,13 @@
 package WebsocketServer.game.model;
 
 import WebsocketServer.game.services.GameBoardService;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
 
+@Component
+@Getter
 public class Player {
-    GameBoard gameBoard;
+    private final GameBoard gameBoard;
 
     public Player(GameBoardService gameBoardService){
         gameBoard = gameBoardService.createGameBoard();
---
+++ src/test/java/WebsocketServer/game/model/PlayerTest.java
@@ -0,0 +1,25 @@
+package WebsocketServer.game.model;
+
+import WebsocketServer.game.services.GameBoardService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PlayerTest {
+    @Autowired
+    Player player;
+    @Autowired
+    GameBoardService gameBoardService;
+
+    @Test
+    void testCreatePlayer(){
+        GameBoard gameBoard = gameBoardService.createGameBoard();
+        assertNotNull(player.getGameBoard());
+        assertTrue(player.getGameBoard().isFinalized());
+    }
+
+}
