909d935b66a17a14c5b4b584893fd6be5f4633ec
jublebi <julianlechner31@gmail.com>
2024-04-21 17:15:59 +0200

    Part of RocketBarometer

--- src/main/java/WebsocketServer/game/model/RocketBarometer.java
+++ src/main/java/WebsocketServer/game/model/RocketBarometer.java
@@ -1,5 +1,44 @@
 package WebsocketServer.game.model;
 
 public class RocketBarometer {
+    private final int MAX_ROCKET = 40;
+    private int rocketCount;
 
+    public RocketBarometer(){
+        rocketCount = 0;
+    }
+
+    public void addRockets(int rockets){
+        rocketCount += rockets;
+    }
+
+    public int getPointsOfRocketBarometer(){
+        int result = 0;
+
+        if(rocketCount >= 0 && rocketCount < 5){
+            result = 0;
+        }else if(rocketCount >= 5 && rocketCount < 9){
+            result = 15;
+        }else if(rocketCount >= 9 && rocketCount < 12){
+            result = 30;
+        }else if(rocketCount >= 12 && rocketCount < 15){
+            result = 45;
+        }else if(rocketCount >= 15 && rocketCount < 18){
+            result = 60;
+        }else if(rocketCount >= 18 && rocketCount < 21){
+            result = 75;
+        }else if(rocketCount >= 21 && rocketCount < 24){
+            result = 90;
+        }else if(rocketCount >= 24 && rocketCount < 27){
+            result = 105;
+        }else if(rocketCount >= 27 && rocketCount < 29){
+            result = 120;
+        }else if(rocketCount >= 29 && rocketCount < 31){
+            result = 135;
+        }else{
+            result = 150;
+        }
+
+        return  result;
+    }
 }
