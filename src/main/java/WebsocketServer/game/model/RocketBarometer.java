4b376da2f324a5d1d2723f0cf0dc044835c66e08
jublebi <julianlechner31@gmail.com>
2024-04-28 10:18:57 +0200

    Added check to RocketBarometer.addRockets
    Now it is also checked if the new rocket count is less or equal to MAX_ROCKET

--- src/main/java/WebsocketServer/game/model/RocketBarometer.java
+++ src/main/java/WebsocketServer/game/model/RocketBarometer.java
@@ -10,36 +10,26 @@ public class RocketBarometer {
 
     public void addRockets(int rockets){
         if(rockets > 0){
-            rocketCount += rockets;
+            if(rocketCount + rockets <= MAX_ROCKET){
+                rocketCount += rockets;
+            }
         }
     }
 
     public int getPointsOfRocketBarometer(){
-        int result = 0;
+        int result;
 
-        if( rocketCount < 5){
-            result = 0;
-        }else if( rocketCount < 9){
-            result = 15;
-        }else if( rocketCount < 12){
-            result = 30;
-        }else if( rocketCount < 15){
-            result = 45;
-        }else if( rocketCount < 18){
-            result = 60;
-        }else if( rocketCount < 21){
-            result = 75;
-        }else if( rocketCount < 24){
-            result = 90;
-        }else if( rocketCount < 27){
-            result = 105;
-        }else if( rocketCount < 29){
-            result = 120;
-        }else if( rocketCount < 31){
-            result = 135;
-        }else{
-            result = 150;
-        }
+        if( rocketCount < 5) result = 0;
+        else if( rocketCount < 9) result = 15;
+        else if( rocketCount < 12) result = 30;
+        else if( rocketCount < 15) result = 45;
+        else if( rocketCount < 18) result = 60;
+        else if( rocketCount < 21) result = 75;
+        else if( rocketCount < 24) result = 90;
+        else if( rocketCount < 27) result = 105;
+        else if( rocketCount < 29) result = 120;
+        else if( rocketCount < 31) result = 135;
+        else result = 150;
 
         return  result;
     }
