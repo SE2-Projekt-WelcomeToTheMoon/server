847be36c7056eec28fae2126c19fffe336c3d1c6
jublebi <julianlechner31@gmail.com>
2024-04-28 10:14:32 +0200

    Fixed Bug
    Added a check if rockets within Method RocketBarometer.addRockets are greater than 0.

--- src/main/java/WebsocketServer/game/model/RocketBarometer.java
+++ src/main/java/WebsocketServer/game/model/RocketBarometer.java
@@ -9,31 +9,33 @@ public class RocketBarometer {
     }
 
     public void addRockets(int rockets){
-        rocketCount += rockets;
+        if(rockets > 0){
+            rocketCount += rockets;
+        }
     }
 
     public int getPointsOfRocketBarometer(){
         int result = 0;
 
-        if(rocketCount >= 0 && rocketCount < 5){
+        if( rocketCount < 5){
             result = 0;
-        }else if(rocketCount >= 5 && rocketCount < 9){
+        }else if( rocketCount < 9){
             result = 15;
-        }else if(rocketCount >= 9 && rocketCount < 12){
+        }else if( rocketCount < 12){
             result = 30;
-        }else if(rocketCount >= 12 && rocketCount < 15){
+        }else if( rocketCount < 15){
             result = 45;
-        }else if(rocketCount >= 15 && rocketCount < 18){
+        }else if( rocketCount < 18){
             result = 60;
-        }else if(rocketCount >= 18 && rocketCount < 21){
+        }else if( rocketCount < 21){
             result = 75;
-        }else if(rocketCount >= 21 && rocketCount < 24){
+        }else if( rocketCount < 24){
             result = 90;
-        }else if(rocketCount >= 24 && rocketCount < 27){
+        }else if( rocketCount < 27){
             result = 105;
-        }else if(rocketCount >= 27 && rocketCount < 29){
+        }else if( rocketCount < 29){
             result = 120;
-        }else if(rocketCount >= 29 && rocketCount < 31){
+        }else if( rocketCount < 31){
             result = 135;
         }else{
             result = 150;
