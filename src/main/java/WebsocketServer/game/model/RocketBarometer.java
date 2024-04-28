3a1c06095e7c553d90d7028ce82287f0b81e550d
jublebi <julianlechner31@gmail.com>
2024-04-28 10:56:29 +0200

    Refactor RocketBarometer.getPointsOfRocketBarometer
    Goal is to remove duplicate code

--- src/main/java/WebsocketServer/game/model/RocketBarometer.java
+++ src/main/java/WebsocketServer/game/model/RocketBarometer.java
@@ -28,18 +28,22 @@ public class RocketBarometer {
     public int getPointsOfRocketBarometer(){
         int result;
 
-        if( rocketCount < 5) result = 0;
-        else if( rocketCount < 9) result = 15;
-        else if( rocketCount < 12) result = 30;
-        else if( rocketCount < 15) result = 45;
-        else if( rocketCount < 18) result = 60;
-        else if( rocketCount < 21) result = 75;
-        else if( rocketCount < 24) result = 90;
-        else if( rocketCount < 27) result = 105;
-        else if( rocketCount < 29) result = 120;
-        else if( rocketCount < 31) result = 135;
+        if( rocketCountSmallerThan(5)) result = 0;
+        else if( rocketCountSmallerThan(9)) result = 15;
+        else if( rocketCountSmallerThan(12)) result = 30;
+        else if( rocketCountSmallerThan(15)) result = 45;
+        else if( rocketCountSmallerThan(18)) result = 60;
+        else if( rocketCountSmallerThan(21)) result = 75;
+        else if( rocketCountSmallerThan(24)) result = 90;
+        else if( rocketCountSmallerThan(27)) result = 105;
+        else if( rocketCountSmallerThan(29)) result = 120;
+        else if( rocketCountSmallerThan(31)) result = 135;
         else result = 150;
 
         return  result;
+    }
+
+    public boolean rocketCountSmallerThan(int number){
+        return rocketCount < number;
     }
 }
