da127a406e1c506e76219d334916396db84a53ea
kaiser.philipp95@icloud.com <130212301+kaiser1101@users.noreply.github.com>
2024-04-29 22:10:13 +0200

    changed list to hashmap and moved method removePlayBySessionID from LobbyService to Lobby

--- src/main/java/WebsocketServer/game/lobby/Lobby.java
+++ src/main/java/WebsocketServer/game/lobby/Lobby.java
@@ -1,9 +1,16 @@
 package WebsocketServer.game.lobby;
 
+import WebsocketServer.services.LobbyService;
+import WebsocketServer.services.user.CreateUserService;
+import WebsocketServer.services.user.UserListService;
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Klasse um die Lobby zu verwalten
@@ -15,10 +22,13 @@ import java.util.List;
 public class Lobby {
 
     private static final int MAX_PLAYERS = 4;
-    private final List<String> userList;
+
+    @Getter
+    public Map<String, CreateUserService> userListMap;
+    private static final Logger logger = LoggerFactory.getLogger(LobbyService.class);
 
     public Lobby() {
-        this.userList = new ArrayList<>();
+        userListMap = new HashMap<>();
     }
 
     /**
@@ -27,10 +37,11 @@ public class Lobby {
      * @return  boolean if player was added
      */
     public boolean addPlayerToLobby(String username) {
-        if (userList.size() < MAX_PLAYERS && !(userList.contains(username))) {
-            userList.add(username);
+        CreateUserService user = UserListService.userList.getUserByUsername(username);
+        if(userListMap.size() < MAX_PLAYERS && !(userListMap.containsKey(username))) {
+            userListMap.put(username, user);
             return true;
-        } else {
+        }else{
             return false;
         }
     }
@@ -40,26 +51,37 @@ public class Lobby {
      * @param username  Username to remove
      * @return  boolean if player was removed
      */
-    public boolean removePlayerFromLobby(String username){
-        if(userList.contains(username)){
-            userList.remove(username);
+
+    public boolean removePlayerFromLobbyByName(String username){
+        CreateUserService user = UserListService.userList.getUserByUsername(username);
+        if(userListMap.containsKey(username)){
+            userListMap.remove(username, user);
             return true;
         }else{
             return false;
+        }
+    }
+    public void removePlayerFromLobbyBySessionID(String sessionID){
+        CreateUserService user = UserListService.userList.getUserBySessionID(sessionID);
+        if(userListMap.containsKey(user.getUsername())){
+            userListMap.remove(user.getUsername(), user);
+            logger.info("User {} removed from lobby: {}", user.getUsername(), user.getSessionID());
+        }else{
+            logger.warn("User {} not in lobby: {}", user.getUsername(), user.getSessionID());
         }
     }
 
     /**
      * Return new Arraylist with users in lobby
      */
-    public ArrayList<String> getUserListFromLobby() {
-        return new ArrayList<>(userList);
+    public ArrayList<CreateUserService> getUserListFromLobby() {
+        return new ArrayList<>(userListMap.values());
     }
 
     /**
      * Remove all players from lobby
      */
     public void removeAllPlayersFromLobby() {
-        userList.clear();
+        userListMap.clear();
     }
 }
