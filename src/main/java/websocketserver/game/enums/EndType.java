11cc629bfa8eccc5804f2cb416a09c705ff3dbda
jublebi <julianlechner31@gmail.com>
2024-06-11 21:34:11 +0200

    Fixed unused variable

--- src/main/java/websocketserver/game/enums/EndType.java
+++ src/main/java/websocketserver/game/enums/EndType.java
@@ -1,12 +1,6 @@
 package websocketserver.game.enums;
 
 public enum EndType {
-    ROCKETS_COMPLETED(0),
-    SYSTEM_ERROR_EXCEEDED(1);
-
-    private final int value;
-
-    EndType(int index) {
-        this.value = index;
-    }
+    ROCKETS_COMPLETED,
+    SYSTEM_ERROR_EXCEEDED;
 }
