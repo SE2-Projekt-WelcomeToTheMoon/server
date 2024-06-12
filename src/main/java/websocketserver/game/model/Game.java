07134874f600b82f48b055299041f5ea55cbd0e8
thomas <t1wobak@edu.aau.at>
2024-06-12 09:44:04 +0200

    Bugfix: Fixed issue with Cards being drawn twice before sending to players

--- src/main/java/websocketserver/game/model/Game.java
+++ src/main/java/websocketserver/game/model/Game.java
@@ -26,7 +26,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -84,7 +83,6 @@ public class Game {
         }
 
         currentPlayerDraw.clear();
-        cardManager.drawNextCard();
 
         for (CreateUserService createUserService : players) {
             if (!createUserService.getGameBoard().checkCardCombination(cardManager.getCurrentCombination())) {
