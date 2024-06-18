f15ecda9097f73e7523712aa823348f56e4d4c02
thomas <t1wobak@edu.aau.at>
2024-06-18 13:30:31 +0200

    Updated Reward Class itself to work with new Requirements

--- src/test/java/websocketserver/game/model/RewardTest.java
+++ src/test/java/websocketserver/game/model/RewardTest.java
@@ -9,48 +9,27 @@ class RewardTest {
     void testRewardConstruction() {
         Reward reward = new Reward(RewardCategory.ROCKET);
         assertEquals(RewardCategory.ROCKET, reward.getCategory());
-        assertFalse(reward.isRocketsFilled());
         assertFalse(reward.isSystemErrorClaimed());
         assertEquals(0, reward.getNumberRockets());
-        assertEquals(0, reward.getUnfilledRockets());
     }
 
     @Test
     void testRewardConstructionWithNumberRockets() {
         Reward reward = new Reward(RewardCategory.ROCKET, 5);
         assertEquals(RewardCategory.ROCKET, reward.getCategory());
-        assertFalse(reward.isRocketsFilled());
         assertFalse(reward.isSystemErrorClaimed());
         assertEquals(5, reward.getNumberRockets());
-        assertEquals(0, reward.getUnfilledRockets());
     }
 
-    @Test
-    void testRewardConstructionWithUnfilledRockets() {
-        Reward reward = new Reward(RewardCategory.UNFILLEDROCKET, 3);
-        assertEquals(RewardCategory.UNFILLEDROCKET, reward.getCategory());
-        assertFalse(reward.isRocketsFilled());
-        assertFalse(reward.isSystemErrorClaimed());
-        assertEquals(0, reward.getNumberRockets());
-        assertEquals(3, reward.getUnfilledRockets());
-    }
+
 
     @Test
     void testRewardConstructionWithInvalidCategory() {
         assertThrows(IllegalArgumentException.class,() -> new Reward(null));
     }
-
-    @Test
-    void testFillRockets() {
-        Reward reward = new Reward(RewardCategory.ROCKET, 3);
-        assertFalse(reward.isRocketsFilled());
-        reward.fillRockets();
-        assertTrue(reward.isRocketsFilled());
-    }
-
     @Test
     void testClaimSystemError() {
-        Reward reward = new Reward(RewardCategory.PLANING); // Any valid category
+        Reward reward = new Reward(RewardCategory.SYSTEMERROR); // Any valid category
         assertFalse(reward.isSystemErrorClaimed());
         reward.claimSystemError();
         assertTrue(reward.isSystemErrorClaimed());
@@ -62,14 +41,5 @@ class RewardTest {
             Reward reward = new Reward(category);
             assertEquals(category, reward.getCategory());
         }
-    }
-
-    @Test
-    void testDefaultFieldValues() {
-        Reward reward = new Reward(RewardCategory.ROCKETFILLING);
-        assertFalse(reward.isRocketsFilled());
-        assertFalse(reward.isSystemErrorClaimed());
-        assertEquals(0, reward.getNumberRockets());
-        assertEquals(0, reward.getUnfilledRockets());
     }
 }
